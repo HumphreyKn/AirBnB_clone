31314343a7fe609a34211fd33d2afde5676675b6
HumphreyKn <kathurimahumphreyz@gmail.com>
2024-02-08 13:19:01 +0300

    [Add] Ability to create  BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,25 @@ import uuid
 class BaseModel:
     """A base class for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize the instance attributes"""
-        # generate a unique id
-        self.id = str(uuid.uuid4())
-        # assign the current datetime
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        # if kwargs is not empty
+        if kwargs:
+            # iterate over the dictionary items
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+
+                # set the attribute with the given key and value
+                setattr(self, key, value)
+        else:
+            # generate a unique id
+            self.id = str(uuid.uuid4())
+            # assign the current datetime
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
